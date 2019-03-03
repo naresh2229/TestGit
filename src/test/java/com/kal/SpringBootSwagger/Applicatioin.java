d15fb2faab4603c898335b2c0fe7f53c2f018eec
naresh2229 <pariginareshkumar@gmail.com>
2019-03-03 11:06:01 +0530

    sample commit

---
+++ src/test/java/com/kal/SpringBootSwagger/Applicatioin.java
@@ -0,0 +1,14 @@
+package com.kal.SpringBootSwagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+
+
+@SpringBootConfiguration
+public class Applicatioin {
+
+	 public static void main( String[] args )
+	    {
+	    	  SpringApplication.run(App.class, args);
+	    }
+}
